6838d50ffcfaa1568331de4e0b0b4c11588c59f0
jdzy <suchengcity@163.com>
2021-09-08 19:41:38 +0800

    MergeList_Sq(SqList La,SqList Lb,SqList &Lc)

---
+++ 01_List/04_线性表合并.cpp
@@ -0,0 +1,18 @@
+void MergeList_Sq(SqList La,SqList Lb,SqList &Lc) {
+    int *pa=La.elem;
+    int *pb=Lb.elem;
+    Lc.listsize=Lc.length=La.length+Lb.length;
+    int *pc=Lc.elem=(int *)malloc(Lc.listsize*sizeof(int));
+
+    int *La_last=La.elem+La.length-1;
+    int *Lb_last=Lb.elem+Lb.length-1;
+
+    while(pa<=La_last && pb<=Lb_last) {
+        if(*pa<=*pb) *pc++=*pa++;
+        else         *pc++=*pb++;
+    }
+
+    while(pa<=La_last) *pc++=*pa++;
+    while(pb<=Lb_last) *pc++=*pb++;
+
+}
